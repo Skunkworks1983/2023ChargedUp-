5ea69e7708e868391da3d0330987e17bbf416d88
bella <bellasoutham@gmail.com>
2023-01-14 14:38:19 -0800

    skeleton commit with some more less bones

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -10,6 +10,5 @@ public class Constants extends CommandBase
 
         public static final int LEFT_JOY_STICK_PORT = 0;
         public static final int RIGHT_JOY_STICK_PORT = 1;
-        public static final int BUTTON_STICK_PORT = 2;
     }
 }
