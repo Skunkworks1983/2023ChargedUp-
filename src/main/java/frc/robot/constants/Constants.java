d5f4c3635806c912f65554ccab1e6f4aa8235e34
bella <bellasoutham@gmail.com>
2023-01-16 15:50:26 -0800

    start to oi code

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -5,11 +5,19 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class Constants extends CommandBase
 {
-    public class JoystickPorts
+    public class OIButtons
     {
-
-        public static final int LEFT_JOY_STICK_PORT = 0;
-        public static final int RIGHT_JOY_STICK_PORT = 1;
-        public static final int BUTTON_STICK_PORT = 2;
+        public static final int TEST_BUTTON_ONE = 2;
+        public static final int TEST_BUTTON_TWO = 3;
+        public static final int TEST_BUTTON_THREE = 4;
+        public static final int TEST_BUTTON_FOUR = 5;
     }
+        public class JoystickPorts
+        {
+
+            public static final int LEFT_JOY_STICK_PORT = 0;
+            public static final int RIGHT_JOY_STICK_PORT = 1;
+            public static final int BUTTON_STICK_PORT = 2;
+        }
 }
+
