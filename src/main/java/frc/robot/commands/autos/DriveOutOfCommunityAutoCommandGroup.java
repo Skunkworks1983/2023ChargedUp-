49aaeca8182e171eb729780e13400c1c1777671a
bella <bellasoutham@gmail.com>
2023-01-16 19:46:53 -0800

    made a command group for driving out of the community in auto

---
+++ src/main/java/frc/robot/commands/autos/DriveOutOfCommunityAutoCommandGroup.java
@@ -0,0 +1,14 @@
+package frc.robot.commands.autos;
+
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class DriveOutOfCommunityAutoCommandGroup extends SequentialCommandGroup
+{
+    public DriveOutOfCommunityAutoCommandGroup(/*Drivebase drivebase*/)
+    {
+        // TODO: Add your sequential commands in the super() call, e.g.
+        //           super(new OpenClawCommand(), new MoveArmCommand());
+        super();
+    }
+}
