4f013e015642505e7a27c9f853f5833c0a0a6ef8
bella <bellasoutham@gmail.com>
2023-01-16 20:49:26 -0800

    started a subsystem from the arm code

---
+++ src/main/java/frc/robot/commands/autos/DriveOutOfCommunityAutoCommandGroup.java
@@ -0,0 +1,14 @@
+package frc.robot.commands.autos;
+
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class DriveOutOfCommunityAutoCommandGroup extends SequentialCommandGroup
+{
+    public DriveOutOfCommunityAutoCommandGroup()
+    {
+        // TODO: Add your sequential commands in the super() call, e.g.
+        //           super(new OpenClawCommand(), new MoveArmCommand());
+        super();
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase
+{
+
+    // With eager singleton initialization, any static variables/fields used in the 
+    // constructor must appear before the "INSTANCE" variable so that they are initialized 
+    // before the constructor is called when the "INSTANCE" variable initializes.
+
+    /**
+     * The Singleton instance of this ArmSubsystem. Code should use
+     * the {@link #getInstance()} method to get the single instance (rather
+     * than trying to construct an instance of this class.)
+     */
+    private final static ArmSubsystem INSTANCE = new ArmSubsystem();
+
+    /**
+     * Returns the Singleton instance of this ArmSubsystem. This static method
+     * should be used, rather than the constructor, to get the single instance
+     * of this class. For example: {@code ArmSubsystem.getInstance();}
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static ArmSubsystem getInstance()
+    {
+        return INSTANCE;
+    }
+
+    /**
+     * Creates a new instance of this ArmSubsystem. This constructor
+     * is private since this class is a Singleton. Code should use
+     * the {@link #getInstance()} method to get the singleton instance.
+     */
+    private ArmSubsystem()
+    {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    }
+}
+
