ff5e28435cf06fc45e8a1ee6a2ce4fe74179dda2
bella <bellasoutham@gmail.com>
2023-01-23 19:04:19 -0800

    updated the wpilib (hopefully)

--- src/main/java/frc/robot/commands/autos/DriveOutOfCommunityAutoCommandGroup.java
+++ src/main/java/frc/robot/commands/autos/DriveOutOfCommunityAutoCommandGroup.java
@@ -2,13 +2,16 @@ package frc.robot.commands.autos;
 
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.drivebase.DriveDistanceCommand;
+import frc.robot.constants.Constants;
+import frc.robot.subsystems.Drivebase;
 
 public class DriveOutOfCommunityAutoCommandGroup extends SequentialCommandGroup
 {
-    public DriveOutOfCommunityAutoCommandGroup()
+    public DriveOutOfCommunityAutoCommandGroup(Drivebase drivebase)
     {
         // TODO: Add your sequential commands in the super() call, e.g.
         //           super(new OpenClawCommand(), new MoveArmCommand());
-        super();
+        super(new DriveDistanceCommand(drivebase, Constants.Drivebase.DRIVE_OUT_OF_COMMUINITY));
     }
 }
