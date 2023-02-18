af767b287d9ba3df4d5be992c0ea39c65ffcee0e
TheBaconPug <thebaconpug@gmail.com>
2023-02-18 15:41:46 -0800

    remove Rotate180Degrees.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,9 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.arm.RotateDegrees;
-import frc.robot.commands.arm.ResetArm;
-import frc.robot.commands.arm.Rotate180Degrees;
 import frc.robot.subsystems.Arm;
 
 
