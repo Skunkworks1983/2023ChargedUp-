5e84c739efda781b29141629cad5497517b28b46
bella <bellasoutham@gmail.com>
2023-03-01 19:17:52 -0800

    merged with main

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.arm.WaveCollectorCommandGroup;
+import frc.robot.commands.autos.SimpleAutoCommandGroup;
 import frc.robot.commands.drivebase.TankDrive;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Arm;
--- src/main/java/frc/robot/commands/autos/LiftArmAndWaveAutoCommandGroup.java
+++ src/main/java/frc/robot/commands/autos/LiftArmAndWaveAutoCommandGroup.java
@@ -4,6 +4,7 @@ package frc.robot.commands.autos;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import frc.robot.commands.arm.SetShoulderSpeed;
+import frc.robot.commands.arm.WaveCollectorCommandGroup;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Arm;
 
