e27018a99ecf50491166c83cbc7da3e93ce901f2
2Carrot10 <eleanorgrutz@gmail.com>
2023-03-14 18:31:34 -0700

    changing branch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -100,12 +100,13 @@ public class Robot extends TimedRobot
     @Override
 
     public void autonomousInit() {
+        Drivebase.GetDrivebase().SetBrakeMode(true);
         Trajectory exampleTrajectoryInMeters =
                 TrajectoryGenerator.generateTrajectory(
                         // Start at the origin facing the +X direction
                         new Pose2d(0, 0, new Rotation2d(0)),
                         // Pass through these two interior waypoints, making an 's' curve path
-                        List.of(new Translation2d(1, .5)),
+                        List.of(new Translation2d(2, 0)),
                         // End 3 meters straight ahead of where we started, facing forward
                         new Pose2d(2, 1, new Rotation2d(0)),
                         // Pass config
@@ -145,6 +146,7 @@ public class Robot extends TimedRobot
     @Override
     public void testInit()
     {
+        Drivebase.GetDrivebase().SetBrakeMode(false);
         // Cancels all running commands at the start of test mode.
         CommandScheduler.getInstance().cancelAll();
     }
